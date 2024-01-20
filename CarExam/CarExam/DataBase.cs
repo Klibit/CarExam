6f90e559bbbdfc9dea9ccc878531603514e49f18
Klibit <kovacsbence720@gmail.com>
2024-01-20 17:41:58 +0100

    DataBase class

---
+++ CarExam/CarExam/DataBase.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+
+namespace CarExam
+{
+    internal class DataBase
+    {
+        MySqlConnection conn = null;
+        MySqlCommand sql = null;
+        DataBase() { 
+        
+        MySqlConnectionStringBuilder builder = new MySqlConnectionStringBuilder();
+            builder.Server = "localhost";
+            builder.UserID = "root";
+            builder.Password = "";
+            builder.Database = "autok";
+            builder.CharacterSet = "utf8";
+            conn = new MySqlConnection(builder.ConnectionString);
+            sql = conn.CreateCommand();
+
+            try
+            {
+                kapcsolatNyit();
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+                Environment.Exit(0);
+            }
+            finally
+            {
+                kapcsolatZar();
+            }
+
+        }
+
+        private void kapcsolatZar()
+        {
+            if (conn.State != System.Data.ConnectionState.Closed) conn.Close();
+        }
+
+        private void kapcsolatNyit()
+        {
+            if (conn.State != System.Data.ConnectionState.Open) conn.Open();
+        }
+    }
+}
