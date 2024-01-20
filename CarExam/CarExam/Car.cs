27a248461fb385312643502ad6cd04bb79cb2620
Klibit <kovacsbence720@gmail.com>
2024-01-20 17:30:44 +0100

    Form Design and Car.cs bases

---
+++ CarExam/CarExam/Car.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarExam
+{
+    internal class Car
+    {
+        string rendszam;
+        string marka;
+        string modell;
+        int gyartasiEv;
+        string forgalmiErvenyesseg;
+        int vetelAr;
+        int kmAllas;
+        int hengerUrtartalom;
+        int tomeg;
+        int teljesitmeny;
+
+        public string Rendszam { get => rendszam; set => rendszam = value; }
+        public string Marka { get => marka; set => marka = value; }
+        public string Modell { get => modell; set => modell = value; }
+        public int GyartasiEv { get => gyartasiEv; set => gyartasiEv = value; }
+        public string ForgalmiErvenyesseg { get => forgalmiErvenyesseg; set => forgalmiErvenyesseg = value; }
+        public int VetelAr { get => vetelAr; set => vetelAr = value; }
+        public int KmAllas { get => kmAllas; set => kmAllas = value; }
+        public int HengerUrtartalom { get => hengerUrtartalom; set => hengerUrtartalom = value; }
+        public int Tomeg { get => tomeg; set => tomeg = value; }
+        public int Teljesitmeny { get => teljesitmeny; set => teljesitmeny = value; }
+
+        public Car(string rendszam, string marka, string modell, int gyartasiEv, string forgalmiErvenyesseg, int vetelAr, int kmAllas, int hengerUrtartalom, int tomeg, int teljesitmeny)
+        {
+            Rendszam = rendszam;
+            Marka = marka;
+            Modell = modell;
+            GyartasiEv = gyartasiEv;
+            ForgalmiErvenyesseg = forgalmiErvenyesseg;
+            VetelAr = vetelAr;
+            KmAllas = kmAllas;
+            HengerUrtartalom = hengerUrtartalom;
+            Tomeg = tomeg;
+            Teljesitmeny = teljesitmeny;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.rendszam} {this.vetelAr}";
+        }
+    }
+}
---
+++ CarExam/CarExam/FormDatas.Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace CarExam
+{
+    partial class FormDatas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxId = new System.Windows.Forms.TextBox();
+            this.labelId = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxRendszam = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxModell = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxMarka = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxgyartasiEv = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxHengerUrtartalom = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxKmAllas = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBoxVetelAr = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBoxForgalmiErvenyesseg = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBoxTeljesitmeny = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBoxTomeg = new System.Windows.Forms.TextBox();
+            this.buttonMuvelet = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxId
+            // 
+            this.textBoxId.Location = new System.Drawing.Point(15, 28);
+            this.textBoxId.Name = "textBoxId";
+            this.textBoxId.Size = new System.Drawing.Size(100, 22);
+            this.textBoxId.TabIndex = 0;
+            // 
+            // labelId
+            // 
+            this.labelId.AutoSize = true;
+            this.labelId.Location = new System.Drawing.Point(12, 9);
+            this.labelId.Name = "labelId";
+            this.labelId.Size = new System.Drawing.Size(20, 16);
+            this.labelId.TabIndex = 1;
+            this.labelId.Text = "ID";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Rendszám";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBoxRendszam
+            // 
+            this.textBoxRendszam.Location = new System.Drawing.Point(15, 109);
+            this.textBoxRendszam.Name = "textBoxRendszam";
+            this.textBoxRendszam.Size = new System.Drawing.Size(100, 22);
+            this.textBoxRendszam.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(146, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Modell";
+            // 
+            // textBoxModell
+            // 
+            this.textBoxModell.Location = new System.Drawing.Point(149, 109);
+            this.textBoxModell.Name = "textBoxModell";
+            this.textBoxModell.Size = new System.Drawing.Size(100, 22);
+            this.textBoxModell.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(281, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Márka";
+            // 
+            // textBoxMarka
+            // 
+            this.textBoxMarka.Location = new System.Drawing.Point(284, 109);
+            this.textBoxMarka.Name = "textBoxMarka";
+            this.textBoxMarka.Size = new System.Drawing.Size(100, 22);
+            this.textBoxMarka.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(412, 90);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Gyártási év";
+            // 
+            // textBoxgyartasiEv
+            // 
+            this.textBoxgyartasiEv.Location = new System.Drawing.Point(415, 109);
+            this.textBoxgyartasiEv.Name = "textBoxgyartasiEv";
+            this.textBoxgyartasiEv.Size = new System.Drawing.Size(100, 22);
+            this.textBoxgyartasiEv.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(281, 161);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(114, 16);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Henger űrtartalom";
+            // 
+            // textBoxHengerUrtartalom
+            // 
+            this.textBoxHengerUrtartalom.Location = new System.Drawing.Point(284, 180);
+            this.textBoxHengerUrtartalom.Name = "textBoxHengerUrtartalom";
+            this.textBoxHengerUrtartalom.Size = new System.Drawing.Size(100, 22);
+            this.textBoxHengerUrtartalom.TabIndex = 16;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(146, 161);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 16);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Km Állás";
+            // 
+            // textBoxKmAllas
+            // 
+            this.textBoxKmAllas.Location = new System.Drawing.Point(149, 180);
+            this.textBoxKmAllas.Name = "textBoxKmAllas";
+            this.textBoxKmAllas.Size = new System.Drawing.Size(100, 22);
+            this.textBoxKmAllas.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(15, 161);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 16);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Vétel ár";
+            // 
+            // textBoxVetelAr
+            // 
+            this.textBoxVetelAr.Location = new System.Drawing.Point(18, 180);
+            this.textBoxVetelAr.Name = "textBoxVetelAr";
+            this.textBoxVetelAr.Size = new System.Drawing.Size(100, 22);
+            this.textBoxVetelAr.TabIndex = 12;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(544, 90);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(142, 16);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Forgalmi érvényesség";
+            // 
+            // textBoxForgalmiErvenyesseg
+            // 
+            this.textBoxForgalmiErvenyesseg.Location = new System.Drawing.Point(547, 109);
+            this.textBoxForgalmiErvenyesseg.Name = "textBoxForgalmiErvenyesseg";
+            this.textBoxForgalmiErvenyesseg.Size = new System.Drawing.Size(139, 22);
+            this.textBoxForgalmiErvenyesseg.TabIndex = 10;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(544, 161);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(84, 16);
+            this.label9.TabIndex = 21;
+            this.label9.Text = "Teljesítmény";
+            // 
+            // textBoxTeljesitmeny
+            // 
+            this.textBoxTeljesitmeny.Location = new System.Drawing.Point(547, 180);
+            this.textBoxTeljesitmeny.Name = "textBoxTeljesitmeny";
+            this.textBoxTeljesitmeny.Size = new System.Drawing.Size(100, 22);
+            this.textBoxTeljesitmeny.TabIndex = 20;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(413, 161);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(51, 16);
+            this.label10.TabIndex = 19;
+            this.label10.Text = "Tömeg";
+            // 
+            // textBoxTomeg
+            // 
+            this.textBoxTomeg.Location = new System.Drawing.Point(416, 180);
+            this.textBoxTomeg.Name = "textBoxTomeg";
+            this.textBoxTomeg.Size = new System.Drawing.Size(100, 22);
+            this.textBoxTomeg.TabIndex = 18;
+            // 
+            // buttonMuvelet
+            // 
+            this.buttonMuvelet.Location = new System.Drawing.Point(284, 286);
+            this.buttonMuvelet.Name = "buttonMuvelet";
+            this.buttonMuvelet.Size = new System.Drawing.Size(203, 88);
+            this.buttonMuvelet.TabIndex = 22;
+            this.buttonMuvelet.Text = "button1";
+            this.buttonMuvelet.UseVisualStyleBackColor = true;
+            // 
+            // FormDatas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonMuvelet);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBoxTeljesitmeny);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.textBoxTomeg);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxHengerUrtartalom);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBoxKmAllas);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBoxVetelAr);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBoxForgalmiErvenyesseg);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxgyartasiEv);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxMarka);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxModell);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxRendszam);
+            this.Controls.Add(this.labelId);
+            this.Controls.Add(this.textBoxId);
+            this.Name = "FormDatas";
+            this.Text = "FormDatas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxId;
+        private System.Windows.Forms.Label labelId;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxRendszam;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxModell;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxMarka;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxgyartasiEv;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxHengerUrtartalom;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxKmAllas;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBoxVetelAr;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBoxForgalmiErvenyesseg;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBoxTeljesitmeny;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBoxTomeg;
+        private System.Windows.Forms.Button buttonMuvelet;
+    }
+}
